170213a53d2f90907278c9bceb49def1bdc49662
Muhammad Ridwan Farizqi <mridwan6661@gmail.com>
2026-04-13 18:54:00 +0700

    accidentally add src to .gitignore

---
+++ src/fetch.h
@@ -0,0 +1,9 @@
+#ifndef FETCH_H
+#define FETCH_H
+
+void get_os();
+void get_kernel();
+void get_hostname();
+void get_shell();
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include "fetch.h"
+
+int main() {
+    get_os();
+    get_kernel();
+    get_hostname();
+    get_shell();
+    return 0;
+}
